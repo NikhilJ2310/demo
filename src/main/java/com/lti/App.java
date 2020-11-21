277297e169ea9b5f6a29694c075455c1e1a86896
NikhilJ2310 <74709382+NikhilJ2310@users.noreply.github.com>
2020-11-21 17:17:54 +0530

    Update App.java

--- src/main/java/com/lti/App.java
+++ src/main/java/com/lti/App.java
@@ -8,6 +8,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        for(int i=0;i<10;i++){
+for(int j=0;j<i;j++){
+System.out.print("(*_*)");
+}
+System.out.println();
+}
     }
 }
